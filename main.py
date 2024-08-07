1320975847e1defc6726800393dbfa8f569b1fab
Justin Kunimune <justinkunimune@gmail.com>
2024-08-06 20:31:58 -0400

    the contour with uncertainty!
    behold!

--- main.py
+++ main.py
@@ -3,44 +3,76 @@ This work by Justin Kunimune is marked with CC0 1.0 Universal.
 To view a copy of this license, visit <https://creativecommons.org/publicdomain/zero/1.0>
 """
 from matplotlib import pyplot as plt
-from numpy import linspace, meshgrid, hypot, exp, random, newaxis, ravel
+from matplotlib.patches import PathPatch
+from matplotlib.path import Path
+from numpy import linspace, meshgrid, hypot, exp, random, newaxis, ravel, mean, quantile
+from skimage import measure
 
 from colormap import colormap
 
-rng = random.default_rng(0)
+def main():
+	rng = random.default_rng(0)
 
-N = 10_000
+	N = 10_000
 
-a = rng.normal(-5, .1, (N, 1, 1))
-b = rng.normal(1, .1, (N, 1, 1))
-r = rng.normal(.6, .01, (N, 1, 1))
-depth = rng.normal(6, .1, (N, 1, 1))
-x0 = rng.normal(-.3, .02, (N, 1, 1))
-y0 = rng.normal(-.1, .02, (N, 1, 1))
-power = 3
-r2 = rng.normal(.2, .01, (N, 1, 1))
-depth2 = rng.uniform(0, 2**(1/2), (N, 1, 1))**2
-x02 = rng.normal(-.2, .02, (N, 1, 1))
-y02 = rng.normal(.4, .02, (N, 1, 1))
+	a = rng.normal(-5, .1, (N, 1, 1))
+	b = rng.normal(1, .1, (N, 1, 1))
+	r = rng.normal(.6, .01, (N, 1, 1))
+	depth = rng.normal(6, .1, (N, 1, 1))
+	x0 = rng.normal(-.3, .02, (N, 1, 1))
+	y0 = rng.normal(-.1, .02, (N, 1, 1))
+	power = 3
+	r2 = rng.normal(.2, .01, (N, 1, 1))
+	depth2 = rng.uniform(0, 2**(1/2), (N, 1, 1))**2
+	x02 = rng.normal(-.2, .02, (N, 1, 1))
+	y02 = rng.normal(.4, .02, (N, 1, 1))
 
-x = y = linspace(-1, 1, 51)
-x = x[newaxis, :, newaxis]
-y = y[newaxis, newaxis, :]
+	x = y = linspace(-1, 1, 51)
+	x = x[newaxis, :, newaxis]
+	y = y[newaxis, newaxis, :]
 
-image = (a*x + b*y -
-         depth*exp(-(hypot(x - x0, y - y0)/r)**power) -
-         depth2*exp(-(hypot(x - x02, y - y02)/r2)**power))
+	image = (a*x + b*y -
+	         depth*exp(-(hypot(x - x0, y - y0)/r)**power) -
+	         depth2*exp(-(hypot(x - x02, y - y02)/r2)**power))
 
-fig, axs = plt.subplots(
-	3, 3, figsize=(5, 5), facecolor="none",
-	gridspec_kw=dict(
-		wspace=0, hspace=0,
+	# plot a few samples from the distribution
+	fig, axs = plt.subplots(
+		3, 3, figsize=(5, 5), facecolor="none",
+		gridspec_kw=dict(
+			wspace=0, hspace=0,
+		)
 	)
-)
-axs = ravel(axs)
-for i in range(9):
-	axs[i].imshow(image[i].T, cmap=colormap)
-	axs[i].contour(image[i].T, colors="w")
-	axs[i].get_xaxis().set_visible(False)
-	axs[i].get_yaxis().set_visible(False)
-plt.show()
+	axs = ravel(axs)
+	for i in range(9):
+		axs[i].imshow(image[i].T, cmap=colormap)
+		axs[i].contour(image[i].T, colors="w")
+		axs[i].get_xaxis().set_visible(False)
+		axs[i].get_yaxis().set_visible(False)
+
+	# plot a contour with uncertainty
+	fig, ax = plt.subplots(facecolor="none")
+	plt.imshow(mean(image, axis=0).T)
+	plot_contour(ax, image, color="w", level=0)
+
+	plt.show()
+
+def plot_contour(ax, images, level, color, credibility=.90, opacity=1):
+	outer_bound = measure.find_contours(quantile(images, 1/2 - credibility/2, axis=0), level)
+	inner_bound = measure.find_contours(quantile(images, 1/2 + credibility/2, axis=0), level)
+	path_sections = outer_bound + [loop[::-1, :] for loop in inner_bound]
+	path_points = []
+	path_commands = []
+	for loop in path_sections:
+		loop_x = loop[:, 0]
+		loop_y = loop[:, 1]
+		path_points += list(zip(loop_x, loop_y))
+		path_commands += [Path.LINETO]*len(loop)
+	path_commands[0] = Path.MOVETO
+	if len(path_points) > 0:
+		ax.add_patch(PathPatch(Path(path_points, path_commands),
+		                       facecolor=color,
+		                       alpha=opacity,
+		                       edgecolor="none"))
+
+if __name__ == "__main__":
+	main()
